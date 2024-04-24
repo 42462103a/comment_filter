5eca691fa1ca7eb22d6c94364a480f892b661526
42462103a <109280586+42462103a@users.noreply.github.com>
2024-04-24 09:42:40 +0800

    Add files via upload

---
+++ project_first/crawler.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import pandas as pd
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.common.exceptions import NoSuchElementException
+
+
+store_list=list(map(str,input("Please enter the name of the locations and separate them with a space:").split()))
+for num in range(len(store_list)): 
+    store=store_list[num]
+
+    driver=webdriver.Chrome()
+    driver.implicitly_wait(2)
+    driver.get("https://www.google.com/maps/@23.4857501,119.4996499,7z?authuser=0&entry=ttu")
+    driver.switch_to.active_element.send_keys(store)
+    driver.switch_to.active_element.send_keys(Keys.RETURN)
+    
+    try:
+        
+        buttons = driver.find_elements(By.CLASS_NAME, "hh2c6")
+        buttons[1].click()
+        scroll = driver.find_element(By.CLASS_NAME,"m6QErb.DxyBCb.kA9KIf.dS8AEf")
+        last_scroll_height = 0
+
+        while True:
+            scroll_height = driver.execute_script("return arguments[0].scrollHeight", scroll)
+            if scroll_height == last_scroll_height:
+                break
+            driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight)", scroll)
+            last_scroll_height = scroll_height
+            time.sleep(0.7)
+        time.sleep(5)
+
+        buttons=driver.find_elements(By.CLASS_NAME,"w8nwRe.kyuRq")
+        for button in buttons:
+            button.click()
+        try:
+            comments= driver.find_elements(By.CSS_SELECTOR, "div[class=\"MyEned\"] span[class=\"wiI7pd\"]")
+        except:
+            comments=[]
+        comments=[comment.text for comment in comments]
+
+
+        stars= driver.find_elements(By.CLASS_NAME,"kvMYJc")
+        ratings=[int(star.get_attribute("aria-label")[0]) for star in stars]
+
+        real_ratings=0
+        for i in range(0,len(comments)):
+            real_ratings+=ratings[i]
+        real_ratings_ave=real_ratings/len(comments)
+        h=len(ratings)-len(comments)
+        comments_long=len(comments)
+
+        for i in range(h):
+            comments.append("")
+
+        comments = pd.Series(comments)
+        stars=pd.Series(stars)
+        combine=pd.DataFrame({"comments":comments,"ratings":ratings})
+        combine.to_csv("google_comments.tsv",sep="\t",index=False)
+        
+        print("Location":,store)
+        print("The real ratings=",real_ratings_ave)
+        print("The amount of comments",len(ratings))
+        print("Review credibility=",'%.2f' %(comments_long/len(ratings)*100),("%"))
+    except:
+        print("Can't fihd",store)
